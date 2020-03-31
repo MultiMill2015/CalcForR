76671a74f8ab4adb27d1073a40a40dab6d46ec73
MultiMill2015 <62934891+MultiMill2015@users.noreply.github.com>
2020-03-31 12:44:35 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -77,29 +77,3 @@ function reset() {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let resultTime = 0
-
-// function getTimeFromPrice(hourCost) {
-//     resultTime = Math.round((document.getElementById("partPrice").value / hourCost) * 100) / 100
-//     document.getElementById("resultHour").innerHTML = resultTime + " Hour(s)";
-//     document.getElementById("resultMinutes").innerHTML = Math.round((resultTime * 60) * 100) / 100 + " Minutes(s)";
-//     document.getElementById("euroHour").innerHTML = "1h = " + hourCost + "€";
-//     // console.log(resultTime)
-//     // console.log(document.getElementById("partPrice").value)
-// };
-
